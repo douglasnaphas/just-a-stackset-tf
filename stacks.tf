5028bc1044fa4d34f698a5d5e73143b5f538f935
Douglas Naphas <douglasnaphas@gmail.com>
2023-01-04 18:13:28 -0500

    Supply the required AdministratorAccountId param
    My last build failed with:
    
    Error: error creating CloudFormation Stack
    (AWSCloudFormationStackSetExecutionRole): ValidationError: Parameters:
    [AdministratorAccountId] must have values status code: 400, request id:
    6cf0c851-fcdd-4b51-b4dd-3583648a2a13
    with aws_cloudformation_stack.AWSCloudFormationStackSetExecutionRole
    on stacks.tf line 7, in resource "aws_cloudformation_stack"
    "AWSCloudFormationStackSetExecutionRole":
    resource "aws_cloudformation_stack"
    "AWSCloudFormationStackSetExecutionRole" {

--- stacks.tf
+++ stacks.tf
@@ -8,4 +8,7 @@ resource "aws_cloudformation_stack" "AWSCloudFormationStackSetExecutionRole" {
   name         = "AWSCloudFormationStackSetExecutionRole"
   template_url = "https://s3.amazonaws.com/cloudformation-stackset-sample-templates-us-east-1/AWSCloudFormationStackSetExecutionRole.yml"
   capabilities = ["CAPABILITY_NAMED_IAM"]
+  parameters = {
+    "AdministratorAccountId" = data.aws_caller_identity.current.account_id
+  }
 }
