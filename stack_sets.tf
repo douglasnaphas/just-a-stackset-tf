45d3c628fb8b1169e3dbf9ad1315fb8764ad2429
Douglas Naphas <douglasnaphas@gmail.com>
2023-01-04 18:59:11 -0500

    Explicitly specify default values for params
    The
    [docs](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudformation_stack_set#parameters) say:
    
    parameters - (Optional) Key-value map of input parameters for the
    StackSet template. All template parameters, including those with a
    Default, must be configured or ignored with lifecycle configuration
    block ignore_changes argument. All NoEcho template parameters must be
    ignored with the lifecycle configuration block ignore_changes argument.

--- stack_sets.tf
+++ stack_sets.tf
@@ -15,6 +15,10 @@ resource "aws_cloudformation_stack_set" "stackset_map" {
   template_body           = file(join("", ["cfn_templates/", replace(each.key, "-", "_"), ".json"]))
   capabilities            = each.value.capabilities
   administration_role_arn = join("", ["arn:aws:iam::", data.aws_caller_identity.current.account_id, ":role/AWSCloudFormationStackSetAdministrationRole"])
+  parameters = {
+    "Param1" = "Param1DefaultValue",
+    "Param2" = "Param2DefaultValue"
+  }
   lifecycle {
     ignore_changes = [
       administration_role_arn
